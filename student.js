6243d4d843237be164934379e83d59d24174d338
idrisraheem <148911135+idrisraheem@users.noreply.github.com>
2026-04-05 09:06:19 +0100

    Update student.js

--- student.js
+++ student.js
@@ -1,58 +1,64 @@
 //data
-let students =[
-{name: "idris", age : 18},
-{name:" fawaz", age : 15},
-{name: "amal", age: 14},
-
+const students=[
+{name :"Idris Abdulraheem", age :22},
+{name :"Fawaz Abdulraheem", age :15},
+{name :"Amal Abdulraheem", age :18}
 ];
-//functions
-function countMinor(students){
-
-return students.filter (s=> s.age < 18).length;
-
-}
-function countAdults(students){
-return students.filter (s=> s.age >= 18).length;
-}
-function getAdults(students){
-  return students.filter(s=> s.age >=18);}
-
-  function getMinors(students){
-    return students.filter(s => s.age < 18);
-    }
-  
-function addStudent(a,student){
-    a.push(student);
-    return a;
-}
-function findOldest(students){
-let oldest = students[0];
-for(let i =0;i<students.length; i++){
-    if(students[i].age>oldest.age){
-        oldest=students[i];
-    }
-    
-}
-return oldest;
-}
-function findYoungest(students){
-    let youngest = students[0];
-    for(let i =0;i<students.length;i++){
-        if(students[i].age < youngest.age){
-            youngest =students[i];
+const student = document.getElementById("student");
+const adult = document.getElementById("adt");
+const youngest = document.getElementById("y");
+const oldest = document.getElementById("o");
+const nameInput = document.getElementById("name");
+const ageInput = document.getElementById("age");
+const addS =document.getElementById("add");
+const output = document.getElementById("output");
+function young(student){
+    let youngest = student[0];
+    for (let i =1; i<student.length; i++){
+        if(student[i].age<youngest.age){
+            youngest = student[i];
         }
     }
-return youngest;
+    return youngest;
 }
-function getAdultNames(students){
-  return student.filter(s=> s.age >=18)
-  .map(s=> s.name);}
-console.log(countAdults(students));
-console.log(countMinor(students));
-console.log(getAdults(students));
-console.log(addStudent(students,{name :"david",age : 19}))
-console.log(findOldest(students));
-console.log(findYoungest(students));
-console.log(getMinors(students));
-console.log(getAdultNames(students));
-console.log(findyoungest(students));
+function old(student){
+    let old = student[0];
+    for (let i=0; i<student.length; i++){
+       if(student[i].age > old.age)
+       {
+        old = student[i];
+       }
+    }
+    return old;
+}
+student.addEventListener("click", function(){
+    output.textContent= students.map(s=> s.name +" - " + s.age).join(", ")
+});
+adult.addEventListener("click",function(){
+    output.textContent = students
+    .filter(s => s.age >= 18)
+    .map(s=> s.name + " - " + s.age)
+    .join(" , ");
+})
+youngest.addEventListener("click",function(){
+    let y = young(students);
+    output.textContent = y.name + " - "+y.age;
+});
+oldest.addEventListener("click", function(){
+
+    let o = old(students);
+    output.textContent = o.name + " - " + o.age;
+})
+addS.addEventListener("click", function(){
+    const name = nameInput.value;
+    const age = Number(ageInput.value);
+    const student= {
+        name : name,
+        age : age
+    }
+    students.push(student);
+    output.textContent = students.map(s=> s.name+ " - " + s.age)
+    .join(", ");
+    nameInput.value =" ";
+    ageInput.value = " ";
+})
